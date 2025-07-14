6ae4eb7c31e650040c6d2e9bb3dca1611d89723f
jshjshjshjsh <cj456456@gmail.com>
2025-07-14 11:41:27 +0900

    add: JWT의 Refresh 토큰 기능 추가

--- src/main/java/com/itjamz/pond_back/security/SecurityConfig.java
+++ src/main/java/com/itjamz/pond_back/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/team/**","/notice/**").hasAnyRole("ADMIN", "NORMAL") // 회원 전용 페이지
-                        .requestMatchers("/","/login", "/member/register").permitAll() // 전체 허용 페이지
+                        .requestMatchers("/","/login", "/login/refresh", "/member/register").permitAll() // 전체 허용 페이지
                         .requestMatchers("/hello", "/test/**").permitAll() // 테스트용
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
