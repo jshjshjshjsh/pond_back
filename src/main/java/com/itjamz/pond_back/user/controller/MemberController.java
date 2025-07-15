afc15594d1e24b8085922d00d8b3d9b46d4a1366
jshjshjshjsh <cj456456@gmail.com>
2025-07-15 19:31:17 +0900

    feat: 소속 팀원들 조회 기능 추가

--- src/main/java/com/itjamz/pond_back/user/controller/MemberController.java
+++ src/main/java/com/itjamz/pond_back/user/controller/MemberController.java
@@ -6,15 +6,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/member")
 public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/member/register")
+    @PostMapping("/register")
     public ResponseEntity<?> memberRegister(@RequestBody Member member) {
         memberService.memberRegister(member);
 
