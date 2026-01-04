31095a25f7884a577cbab4619bebaa6048fa2eff
jshjshjshjsh <cj456456@gmail.com>
2026-01-04 18:13:20 +0900

    test: docker 종속성 제거 시도

--- src/test/java/com/itjamz/pond_back/security/controller/AuthControllerTest.java
+++ src/test/java/com/itjamz/pond_back/security/controller/AuthControllerTest.java
@@ -28,7 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class AuthControllerTest extends AbstractContainerBaseTest {
+class AuthControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
