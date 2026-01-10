d3c9ff28ecb0d2d6cd2adf11afaf2923522e4c0f
jshjshjshjsh <cj456456@gmail.com>
2026-01-10 12:10:33 +0900

    fix: MemberTeamRepository PK 변경

--- src/main/java/com/itjamz/pond_back/user/repository/MemberTeamJpaRepository.java
+++ src/main/java/com/itjamz/pond_back/user/repository/MemberTeamJpaRepository.java
@@ -1,11 +1,12 @@
 package com.itjamz.pond_back.user.repository;
 
 import com.itjamz.pond_back.user.domain.entity.MemberTeam;
+import com.itjamz.pond_back.user.domain.entity.MemberTeamId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface MemberTeamJpaRepository extends JpaRepository<MemberTeam, Long> {
+public interface MemberTeamJpaRepository extends JpaRepository<MemberTeam, MemberTeamId> {
     Optional<MemberTeam> findMemberTeamByIdMember(String sabun);
 }
